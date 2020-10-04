86e60275afffdf035e7bb2dba26f1813ffa58ad1
badawi98 <badawi1998@icloud.com>
2020-10-04 12:37:10 +0300

    add localstorage

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 var inputItem = document.getElementById("inputItem");
 inputItem.focus();
+
+var index = 0 ;
+var data = [];
+
+var getItemIndex = 0 ;
+while(true){
+  var key = "data" + getItemIndex;
+  var value = localStorage.getItem(key);
+  if(value == 'empty0.'){
+    getItemIndex++;
+     continue;
+  }
+  if(value == null || value == "null" || value =='undefined') break;
+  var textbox = document.createElement("input");
+  textbox.type = 'text';
+  textbox.setAttribute("value", value);
+  addItem('sortable' , textbox)
+  getItemIndex++;
+}
+
+
+
+
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("fa fa-trash fa-lg");
@@ -14,6 +31,10 @@ for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
+    var item = div.getElementsByClassName('Badawi')[0].innerText;
+    var index = data.indexOf(item);
+    data[index] = 'empty0.';
+    console.log(data);
   }
 }
 
@@ -45,11 +66,10 @@ listItem.setAttribute("class", "box");
   var label = document.createElement("label");
   var labelText = document.createElement("label");
   label.setAttribute("style", "margin-right: 400px;height: 30px;width: 150px;");
-  labelText.setAttribute("ondblclick", "this.contentEditable=true;this.className='inEdit';");
-  labelText.setAttribute("onblur", "this.contentEditable=false;this.className='';");
-  labelText.setAttribute("contenteditable", "flase");
+  label.setAttribute("contenteditable" , "true");
+  labelText.classList = "Badawi"
   labelText.innerText = input.value;
-
+  
   // Configure the check box
   var checkBox = document.createElement("input");
   checkBox.type = 'checkbox';
@@ -62,13 +82,14 @@ listItem.setAttribute("class", "box");
   // Put the checkbox and label text in to the label element
   label.appendChild(checkBox);
   label.appendChild(labelText);
+  data[index] = input.value;
+  index++;
   
   // Put the label (with the checkbox inside) and the delete
   // button into the list item.
   listItem.appendChild(label);
   listItem.appendChild(deleteButton);
   var newline = document.createElement("hr");
-
   listItem.appendChild(newline);
   list.appendChild(listItem);
   inputItem.focus();
@@ -77,7 +98,24 @@ listItem.setAttribute("class", "box");
 }
 
 function deleteAll(){
-  document.getElementById("sortable").innerHTML = "";
-
+  document.getElementById("sortable").innerHTML = "<hr>";
+  data = [];
+  var key = 'data0';
+  var value = null;
+  localStorage.setItem(key , value);
 }
+
+function saveTolocalStorage(){
+  var length = data.length;
+  for(var i = 0 ; i < length ; i++){
+    var key = 'data'+i;
+    var value = data[i];
+    localStorage.setItem(key , value);
+  }
+}
+
+
+
+
+
 
